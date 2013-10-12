8fa26651f05a28d9734ed71414d23c447fcb3e81
kindadodgy <kindadodgy@gmail.com>
2013-10-12 16:33:38 -0400

    Add files from old site

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* Author:
+
+*/
+
+  jQuery(function($){
+        $(".twitter").tweet({
+          username: "kindadodgy",
+          count: 5,
+          loading_text: "loading tweets..."
+        });
+      });
+
+
+
